d49e63862f3121114647f6ee58e5a326b4026ff8
root <root@localhost.localdomain>
2017-06-29 12:55:23 +0300

    da files wew updated

--- telnet-fact.cpp
+++ telnet-fact.cpp
@@ -5,15 +5,16 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
 
-char buffer[512];
+char buffer[64];
 
 int factor(int src)
 {
-if ((src==0)||(src==1))
-return 1;
-else
-return src*factor(src-1);
+   if ((src==0)||(src==1))
+      return 1;
+   else
+      return src*factor(src-1);
 }
 
 int main()
@@ -23,13 +24,12 @@ int socket1, listener;
 struct sockaddr_in addr;
 int bytes_read;
 
-
 listener = socket(AF_INET, SOCK_STREAM, 0);
 
 if (listener < 0)
 {
-perror("socket");
-exit(1);
+   perror("socket");
+   exit(1);
 }
 
 addr.sin_family = AF_INET;
@@ -37,35 +37,49 @@ addr.sin_port = htons(3425);
 addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 if(bind(listener, (struct sockaddr *) &addr, sizeof(addr))<0)
 {
-perror("bind");
-exit(2);
+   perror("bind");
+   exit(2);
 }
 
 listen(listener,1);
 
 while(1)
 {
-socket1 = accept(listener, NULL, NULL);
-if (socket1 < 0)
-{
-perror("accept");
-exit(3);
-}
+   socket1 = accept(listener, NULL, NULL);
+   if (socket1 < 0)
+   {
+      perror("accept");
+      exit(3);
+   }
 
 while(1)
 {
-bytes_read = recv(socket1, buffer, 512, 0);
-if (bytes_read < 0) break;
+   memset(buffer,0,sizeof(buffer));
+   bytes_read = recv(socket1, buffer, 512, 0);
+   if (bytes_read < 0) break;
+   bool correct=false;
+   for (int i = 0; i <= sizeof(buffer); i++)
+   {
+      if((buffer[i] >='0')&&(buffer[i] <='9')) correct = true;
+   }
+   if (correct == false)
+   {
+       send(socket1,"Wrong input!\n",13,0);
+       continue;
+   }
 
-int temp = atoi(buffer);
-if (temp >= 0)
-{
-int fact = factor(temp);
-char fact1[16];
-sprintf(fact1," %d", fact);
-send(socket1,fact1,sizeof(fact1),0);
-}
-else send(socket1, "Wrong input!\n", 13, 0);
+   int temp = strtol(buffer,NULL,10);
+
+   if (temp >= 0)
+   {
+      int fact = factor(temp);
+      char fact1[512];
+      memset(fact1,0,sizeof(fact1));
+      sprintf(fact1," %d", fact);
+      send(socket1,fact1,sizeof(fact1),0);
+      send(socket1,"\n",1,0);
+   }
+   else send(socket1, "Wrong input!\n", 13, 0);
 }
 }
 return 0;
